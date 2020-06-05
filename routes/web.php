3087d91109b1534007a3a8410a3a4f2a025cbd4e
Muh Fernanda Hardiyanto <53845460+MuhFernandaHardiyanto@users.noreply.github.com>
2020-06-05 16:55:06 +0700

    menambahkan URL

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,45 @@ $router->get('/', function () use ($router) {
 $router->get('/key', function(){
     return str_random(32);
 });
+
+//Membuat Url get dengan "/tesip"
+$router->get('/tesip', function(){
+    return 'Hello world! - GET';
+});
+
+//Membuat Url Post
+$router->post('/bar', function(){
+    return 'Hello World - POST';
+});
+
+//The Route allows you to Register routes that respond to any HTTP verb:
+
+//Membuat Url GET
+$router->get('/get', function(){
+    return 'GET';
+});
+
+//Membuat Url POST
+$router->post('/post', function(){
+    return 'POST';
+});
+
+//Membuat Url PUT
+$router->put('/put', function(){
+    return 'PUT';
+});
+
+//Membuat Url PATCH
+$router->patch('/patch', function(){
+    return 'PATCH';
+});
+
+//Membuat Url DELETE
+$router->delete('/delete', function(){
+    return 'DELETE';
+});
+
+//Membuat Url OPTIONS
+$router->options('/options', function(){
+    return 'OPTIONS';
+});
